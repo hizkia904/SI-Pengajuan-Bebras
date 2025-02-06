a44a52797cf95bf72575facdbd03ed5130e0d27a
hizkia15 <hizkiamanurung06@gmail.com>
2025-02-06 23:08:46 +0700

    hide page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,20 @@ import { Carousel, Layout } from "antd";
 const { Content, Header } = Layout;
 
 export default function Page() {
-  const contentStyleCarousel: React.CSSProperties = {
-    height: "300px",
-    // width: "100vw",
-    // maxHeight: "100%",
-    // maxWidth: "100%",
-    color: "#fff",
-    lineHeight: "300px",
-    textAlign: "center",
-    background: "#1677ff",
-    margin: 0,
-  };
+  // const contentStyleCarousel: React.CSSProperties = {
+  //   height: "300px",
+  //   // width: "100vw",
+  //   // maxHeight: "100%",
+  //   // maxWidth: "100%",
+  //   color: "#fff",
+  //   lineHeight: "300px",
+  //   textAlign: "center",
+  //   background: "#1677ff",
+  //   margin: 0,
+  // };
   return (
     <>
-      <Layout style={{ height: "100vh" }}>
+      {/* <Layout style={{ height: "100vh" }}>
         <Header></Header>
         <Content style={{ background: "#ff" }}>
           <Carousel autoplay arrows infinite={false}>
@@ -36,7 +36,7 @@ export default function Page() {
             </div>
           </Carousel>
         </Content>
-      </Layout>
+      </Layout> */}
     </>
   );
 }
